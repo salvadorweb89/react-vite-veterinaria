7f7e3881803a5a83e59719c7fe972d68811397d9
salvador.ruiz <salvadorweb89@gmail.com>
2023-08-08 19:05:52 +0200

    Implementamos state para cada campo del formulario

--- react-citas/src/components/Formulario.jsx
+++ react-citas/src/components/Formulario.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 
 const Formulario = () => {
+  const [nombreMascota, setNombreMascota] = useState('');
+  const [nombrePropietario, setNombrePropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fechaAlta, setFechaAlta] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
   return (
-    <div className="sm:w-full md:w-1/2 lg:w-2/5">
+    <div className="sm:w-full md:w-1/2 lg:w-2/5 sm:mb-10">
       <h2 className="font-bold text-center text-3xl">Seguimiento de pacientes</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Añadir y administrar pacientes.
@@ -10,23 +16,41 @@ const Formulario = () => {
       <form className="bg-white shadow-md p-5 rounded-md">
         <div className="mb-5">
           <label htmlFor="input-nombre-mascota" className="block">Nombre mascota</label>
-          <input id="input-nombre-mascota" className="w-full border-2 p-2 mt-2 rounded-md" type="text" placeholder="Nombre de la mascota"/>
+          <input id="input-nombre-mascota" className="w-full border-2 p-2 mt-2 rounded-md" 
+          type="text" placeholder="Nombre de la mascota" 
+          value={nombreMascota}
+          onChange={ (e) => setNombreMascota(e.target.value) } 
+          />
         </div>
         <div className="mb-5">
           <label htmlFor="input-nombre-propietario" className="block">Nombre propietario</label>
-          <input id="input-nombre-propietario" className="w-full border-2 p-2 mt-2 rounded-md" type="text" placeholder="Nombre del propietario"/>
+          <input id="input-nombre-propietario" className="w-full border-2 p-2 mt-2 rounded-md" 
+          type="text" placeholder="Nombre del propietario"
+          value={nombrePropietario}
+          onChange={ (e) => setNombrePropietario(e.target.value) } 
+          />
         </div>
         <div className="mb-5">
           <label htmlFor="input-email" className="block">Email</label>
-          <input id="input-email" className="w-full border-2 p-2 mt-2 rounded-md" type="email" placeholder="Email"/>
+          <input id="input-email" className="w-full border-2 p-2 mt-2 rounded-md" 
+          type="email" placeholder="Email"
+          value={email}
+          onChange={ (e) => setEmail(e.target.value) } 
+          />
         </div>
         <div className="mb-5">
           <label htmlFor="input-fecha-alta" className="block">Fecha de alta</label>
-          <input id="input-fecha-alta" className="w-full border-2 p-2 mt-2 rounded-md" type="date" placeholder="Email"/>
+          <input id="input-fecha-alta" className="w-full border-2 p-2 mt-2 rounded-md" 
+          type="date" value={fechaAlta}
+          onChange={ (e) => setFechaAlta(e.target.value) } 
+          />
         </div>
         <div className="mb-5">
           <label htmlFor="textarea-sintomas" className="block">Síntomas</label>
-          <textarea id="textarea-sintomas" className="w-full border-2 p-2 mt-2 rounded-md" placeholder="Describe los síntomas" />
+          <textarea id="textarea-sintomas" className="w-full border-2 p-2 mt-2 rounded-md" 
+          placeholder="Describe los síntomas" 
+          value={sintomas}
+          onChange={ (e) => setSintomas(e.target.value) } />
         </div>
         <div className="mb-5">
           <input type="submit" className="bg-indigo-600 w-full p-2 uppercase text-white font-bold cursor-pointer
