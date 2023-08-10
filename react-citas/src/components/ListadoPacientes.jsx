92cc3744a97dc3f0bf0552847a6e5c038b6f8b94
salvador.ruiz <salvadorweb89@gmail.com>
2023-08-10 18:23:36 +0200

    Implementamos texto condicional en función de si hay pacientes registrados o no

--- react-citas/src/components/ListadoPacientes.jsx
+++ react-citas/src/components/ListadoPacientes.jsx
@@ -1,17 +1,32 @@
 import PropTypes from 'prop-types';
 import Paciente from './Paciente';
 const ListadoPacientes = ({pacientes}) => {
+
+  
+
   return (
     <div className="sm:w-full md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
       <h2 className="font-bold text-center text-3xl">Listado Pacientes</h2>
-      <p className="text-lg mt-5 text-center mb-10">Administra tus pacientes.</p>
-      
-      {pacientes.map(paciente => (
-        <Paciente 
-          key={paciente.id}
-          paciente={paciente}
-        />
-      ))}
+
+      {pacientes && pacientes.length ? 
+        (
+          <>
+            <p className="text-lg mt-5 text-center mb-10">Administra tus pacientes.</p>
+            {pacientes.map(paciente => (
+              <Paciente 
+                key={paciente.id}
+                paciente={paciente}
+              />
+            ))}
+          </>
+        ) : 
+        (
+          <p className="text-lg mt-5 text-center mb-10">
+            No hay pacientes registrados. <br />
+            Empieza <span className="text-indigo-600">agregando pacientes</span> desde el formulario.
+          </p>
+        ) 
+      }
     </div>
   );
 }
