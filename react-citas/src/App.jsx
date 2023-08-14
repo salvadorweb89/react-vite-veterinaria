54f68b5d0d2a751ec33a36af3fabcaa27dec5933
salvador.ruiz <salvadorweb89@gmail.com>
2023-08-14 13:23:24 +0200

    Almacenamos pacientes de localstorage en State pacientes. - Implementamos useEffect para ello. - Encapsulamos lógica de carga en fn cargarLocalStorage. - Implementamos State reRendered para controlar el re-render de React18 y strictmode + modo dev en main.jsx - El problema detectado depurando, es que el State Pacientes no cambia inmediatamente. - El useEffect para Pacientes definido a continuación, se lanza porque ha detectado un intento de cambio sobre Pacientes, pero realmente no tiene el valor que hemos asignado. - Para evitar que se pise localStorage con datos vacíos, comprobamos State reRendered. - Lo marcamos a true junto a nuestra implementación,  y comprobamos al momento de leer si llega true.  - De esta forma me aseguro que estoy trabajando con el State definitivo  - Según he podido leer, esto no ocurre en modo prod  - Incluímos config para debugger con launch.json

--- react-citas/src/App.jsx
+++ react-citas/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -6,6 +6,29 @@ import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   const [pacientes, setPacientes] = useState([]); // State para guardar pacientes
   const [paciente, setPaciente] = useState({}); // State para modificar pacientes
+  const [reRendered, setReRendered] = useState(false); // State para controlar re-render de strictmode React18
+  
+  // UseEffect para ejecutar cuando el component esté listo.
+  useEffect(() => {
+
+    // Cargamos en el State pacientes lo que tenga el localStorage pacientes, cuando el component esté listo
+    const cargarLocalStorage = () => {
+      const pacientesStorage = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesStorage);
+      setReRendered(true);
+    }
+
+     cargarLocalStorage();
+    
+  }, []);
+
+  // Almacenamos en localStorage los cambios en el State pacientes
+  useEffect(() => {
+    // Si tenemos el valor true de reRendered, sabemos que ya contamos con el State definitivo tras rerender
+    if(reRendered) {
+      localStorage.setItem('pacientes', JSON.stringify(pacientes));
+    }
+  }, [pacientes, reRendered]);
 
   const eliminarPaciente = id => {
     const pacientesUpdated = pacientes.filter(paciente => paciente.id != id);
